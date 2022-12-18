e719b69e37580cfaf8a3cdc43f4b9566f9233c13
T-tingting <74326634+T-tingting@users.noreply.github.com>
2022-12-18 14:12:06 -0500

    Add files via upload

---
+++ header.php
@@ -0,0 +1,27 @@
+ 
+<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="icon" href="images/icon.ico">
+	<title>  K.T.U Student Clearance Portal| Registration </title>
+	</head>
+	<body>
+		<header>
+			<div class="container">
+				<div class="branding">
+				<a href="index.php"><img src="images/ktu_logo.png"></a>
+				<h1> <span class="current"> K.T.U </span> Student Clearance Portal </h1>
+			<nav>
+				<ul>
+					<li><a id="h" href="index.php">Home </a> </li>
+					<li><a id="h" href="about.php">About </a></li>
+					<li><a id="h" href="contact.php">Contact </a></li>
+
+				</ul>
+			</nav>
+			</div>
+			</div>
+		</header>
+
+
+
